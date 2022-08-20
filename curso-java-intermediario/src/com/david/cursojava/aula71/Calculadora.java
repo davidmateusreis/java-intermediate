309333e91ad32ba6a26b92add27b67741957f7a9
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-08-20 09:19:56 -0300

    Aula 71: Threads: métodos e blocos sincronizados (synchronized)

---
+++ curso-java-intermediario/src/com/david/cursojava/aula71/Calculadora.java
@@ -0,0 +1,28 @@
+package com.david.cursojava.aula71;
+
+public class Calculadora {
+
+    private int soma;
+
+    public synchronized int somaArray(int[] array) { // apenas uma thread pode acessar esse método por vez
+
+        soma = 0;
+
+        for (int i = 0; i < array.length; i++) {
+
+            soma += array[i];
+
+            System.out.println("Executando a soma " + Thread.currentThread().getName() + " somando o valor " + array[i]
+                    + " com total de " + soma);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+            }
+        }
+
+        return soma;
+    }
+}
