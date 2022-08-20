4d982705fa5afdeedd10474e0a105372fe7036b4
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-08-20 09:44:24 -0300

    Aula 72: Threads: notify, wait e notifyAll

---
+++ curso-java-intermediario/src/com/david/cursojava/aula72/TiqueTaque.java
@@ -0,0 +1,54 @@
+package com.david.cursojava.aula72;
+
+public class TiqueTaque {
+
+    boolean tique;
+
+    synchronized void tique(boolean estaExecutando) {
+
+        if (!estaExecutando) {
+            tique = true;
+            notify();
+            return;
+        }
+
+        System.out.println("Tique ");
+
+        tique = true;
+
+        notify();
+
+        while (tique) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+            }
+        }
+    }
+
+    synchronized void taque(boolean estaExecutando) {
+
+        if (!estaExecutando) {
+            tique = false;
+            notify();
+            return;
+        }
+
+        System.out.println("Taque ");
+
+        tique = false;
+
+        notify();
+
+        while (!tique) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+            }
+        }
+    }
+}
