e4ffb38f425883dda2d81ba34be956f31ac2b14f
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-08-24 10:11:42 -0300

    Aula 100: Instalando jars no Projeto

---
+++ curso-java-intermediario/src/com/david/cursojava/aula85_100/Aula100.java
@@ -0,0 +1,11 @@
+package com.david.cursojava.aula85_100;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Aula100 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println( StringUtils.capitalize("david"));
+    }
+}
